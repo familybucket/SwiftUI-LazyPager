626763a6ac3835b39db6c0c74844f3c1e02f5435
iwdael <4884280@qq.com>
2025-06-15 12:24:43 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "LazyPager",
+    name: "ImageViewer",
     platforms: [
            .iOS(.v15)
        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LazyPager",
+            name: "ImageViewer",
             targets: ["LazyPager"]),
     ],
     dependencies: [
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "LazyPager",
+            name: "ImageViewer",
             dependencies: []),
         .testTarget(
-            name: "LazyPagerTests",
-            dependencies: ["LazyPager"]),
+            name: "ImageViewerTests",
+            dependencies: ["ImageViewer"]),
     ]
 )
